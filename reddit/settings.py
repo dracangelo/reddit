a06d6645f7ac5ed1f8386b00a9577d480c7c2002
dracangelo <kelvinmbuguaw@gmail.com>
2020-02-19 10:43:11 +0300

    added a base index html

--- reddit/settings.py
+++ reddit/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'redditor'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'reddit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reddit2',
+        'USER': 'moringa',
+    'PASSWORD':'Securedd',
     }
 }
 
