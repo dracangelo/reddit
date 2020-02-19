0918adba3006da149153bdb6c8fbd7ac216f4775
dracangelo <kelvinmbuguaw@gmail.com>
2020-02-19 12:17:14 +0300

    added a forms.py

--- redditor/admin.py
+++ redditor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Profile, Comment
+
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
